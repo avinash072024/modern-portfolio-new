12a07afbb4b39f09eaa53ed83674ad30c1c61309
avinash072024 <www.avi.marbhal@gmail.com>
2026-09-19 00:45:34 +0530

    API Url changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://localhost:5000/api',
-    apiUrl: 'https://portfolio-backend-75va.onrender.com/api',
+    apiUrl: 'https://portfolio-backend-skax.onrender.com/api',
     siteUrl: 'https://avinash-modern-portfolio.netlify.app',
     /** GA4 Measurement ID (e.g. G-XXXXXXXXXX). Leave empty to disable analytics. */
     googleAnalyticsId: 'G-978CQZM6DF',
